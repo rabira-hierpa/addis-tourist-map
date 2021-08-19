1e9b9a04f44b51420a5aa236708d75ac32bc952e
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-08-19 12:22:03 +0300

    ✅ updated routes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,13 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/users");
 const pinRoute = require("./routes/pins");
+const attractionsRoute = require('./routes/attractions')
+const artworkRoute = require('./routes/artworks')
+const guesthouseRoute = require('./routes/guesthouses')
+const hotelRoute = require('./routes/hotels')
+const museumRoute = require('./routes/museums')
+
+
 
 dotenv.config();
 
@@ -19,10 +26,16 @@ mongoose
 
 app.use("/api/users", userRoute);
 app.use("/api/pins", pinRoute);
+app.use("/api/attractions",attractionsRoute);
+app.use("/api/artworks",artworkRoute);
+app.use("/api/guesthouses",guesthouseRoute);
+app.use("/api/hotels",hotelRoute);
+app.use("/api/museums",museumRoute);
+
 app.use('/',(req, res)=>{
   res.send("Welcome to Addis Tourist API!!!")
 })
 const port = process.env.PORT || 8080
 app.listen(port, () => {
-  console.log("Backend server is running!");
+  console.log("Backend server is running on port " + port);
 });
