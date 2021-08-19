158536fdf1d15f84c455d16e48203c36c1be5b01
Rabra <rabira.hierpa@gmail.com>
2021-08-19 03:54:04 +0300

    🎉 initial commit

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose 
+ .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,   })   
+ .then(() => console.log("MongoDB connected!"))
+ .catch(err => console.log(err));
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
+app.use('/',(req, res)=>{
+  res.send("Welcome to Addis Tourist API!!!")
+})
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  console.log("Backend server is running!");
+});
